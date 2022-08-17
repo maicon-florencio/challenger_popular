bd2f421873a7c831a895e89cf62768d6dff288bf
maicon.florencio <maicon.florencio@basis.com.br>
2022-08-16 21:46:58 -0300

    primeiro commit criacao de pacote

---
+++ sweethome/src/main/java/com/ch/home/sweethome/SweethomeApplication.java
@@ -0,0 +1,13 @@
+package com.ch.home.sweethome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweethomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweethomeApplication.class, args);
+	}
+
+}
